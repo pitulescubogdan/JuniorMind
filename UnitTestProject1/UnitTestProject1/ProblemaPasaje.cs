bcbe48af1e0b0ffc87b65a4de6d2f7ffdc391186
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-13 01:51:55 +0200

    Add ProblemaPasajeRezolvat

---
+++ UnitTestProject1/UnitTestProject1/ProblemaPasaje.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemeJuniorMind
+{
+    [TestClass]
+    public class ProblemaPasaje
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double result = CalculPietre(23, 25, 5);
+            Assert.AreEqual(25, result);
+        }
+
+        double CalculPietre(int lungime, int latime, int laturaPiatra)
+        {
+            double resultLungime, resultLatime;
+            resultLungime = (double)lungime / laturaPiatra;
+            resultLatime = (double)latime / laturaPiatra;
+
+            return (Math.Ceiling(resultLatime)) * Math.Ceiling(resultLungime);
+        }
+    }
+}
