1a5c33ca4b1d53665d2ab8ce1f34996143558145
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-13 02:30:44 +0200

    RezolvareProblemaParchet

---
+++ ProblemaParchet/ProblemaParchet/ProblemaParchet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemaParchet
+{
+    [TestClass]
+    public class ProblemaParchet
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double result = calculPlaciParchet(3, 3, 1, 0.5);
+            Assert.AreEqual(12, result);
+        }
+        double calculPlaciParchet(int lungimeCamera,int latimeCamera, double lungimeBucataParchet,double latimeBucataParchet)
+        {
+            double dimensiuneCamera = lungimeCamera * latimeCamera;
+            double dimensiuneParchet = lungimeBucataParchet * latimeBucataParchet;
+            double nrBucatiPlaci = dimensiuneCamera / dimensiuneParchet;
+            double pierderiParchet = 0.15 * Math.Ceiling(nrBucatiPlaci);//15% din placile de parchet
+            double dimensiuneParchetnecesar = Math.Ceiling(nrBucatiPlaci) * dimensiuneParchet + (pierderiParchet);
+
+            return Math.Ceiling(dimensiuneParchetnecesar);
+        }
+    }
+}
