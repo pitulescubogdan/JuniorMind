51975c9925a77c2f5971a8cfdfafa1949c9c39ae
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-13 03:21:04 +0200

    ProblemaSportivRezultat

---
+++ ProblemaSportiv/ProblemaSportiv/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemaSportiv
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = calculRepetitii(4);
+            Assert.AreEqual(16, result);
+        }
+        public int calculRepetitii(int runde)
+        {
+            int sum = 0;
+            for(int i = 1;i <= runde; i++)
+            {
+                sum = sum + i;
+            }
+            int repetitii =2*(sum)-runde;
+
+            return repetitii;
+        }
+    }
+}
