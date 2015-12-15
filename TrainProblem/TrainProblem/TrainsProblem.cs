64ac0ad386f78fb8eb674e70048b3e87173802b6
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-15 18:26:42 +0200

    TrainsProblemFirstDistance

---
+++ TrainProblem/TrainProblem/TrainsProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrainProblem
+{
+    [TestClass]
+    public class TrainsProblem
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(60, CalculateDistance(90));
+        }
+
+        public double CalculateDistance(double totalDistance)
+        {    
+            double birdDistance = (totalDistance * 2) / 3;
+            return birdDistance;
+        }
+
+    }
+}
