42a3f5f609a76c56e4de5155e64c77c812d1b545
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-15 21:16:54 +0200

    BankRateForTwoMonths

---
+++ BankRate/BankRate/BankRate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankRate
+{
+    [TestClass]
+    public class BankRate
+    {
+        [TestMethod]
+        public void BankRateForOneMonth()
+        {
+            Assert.AreEqual(197m, CalculateRate(40000, 7.57, 240));
+        }
+        [TestMethod]
+        public void BankRateForTwoMonth()
+        {
+            Assert.AreEqual(56, CalculateRate(100, 10,2));
+        }
+        public decimal CalculateRate(decimal totalSum, double interestPerYear,decimal monthss)
+        {
+            decimal sumPerMonth = totalSum / monthss;
+            decimal interestForFirstMonth = (decimal) (interestPerYear / 100) * totalSum;
+            decimal firstMonth =  sumPerMonth + interestForFirstMonth;
+            decimal interestForSecondMonth =(decimal) interestPerYear / 100 * firstMonth;
+            decimal secondMonth = sumPerMonth + interestForSecondMonth;
+            decimal interestForAnyMonth = (decimal)(interestPerYear / 100) * secondMonth;
+            decimal anyMonth = sumPerMonth + interestForAnyMonth;
+            
+            return anyMonth;
+        }
+    }
+}
--- CiuperciProblem/CiuperciProblem/UnitTest1.cs
+++ CiuperciProblem/CiuperciProblem/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace CiuperciProblem
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculateForTwentyMushrooms()
         {
             Assert.AreEqual(15, CalculCiuperci(20,5));
         }
