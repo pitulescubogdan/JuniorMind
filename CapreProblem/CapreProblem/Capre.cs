37d0e336e2689b16f20e6f26be5efa5b277eca90
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-15 19:50:23 +0200

    ResultGoatsProblem

---
+++ CapreProblem/CapreProblem/Capre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CapreProblem
+{
+    [TestClass]
+    public class Capre
+    {
+        [TestMethod]
+        public void CalculateForTenGoatsPerDay()
+        {
+            Assert.AreEqual(10, CalculFanCapre(1,10));
+        }
+        [TestMethod]
+        public void CalculateForFiveGoatsForFourDays()
+        {
+            Assert.AreEqual(20, CalculFanCapre(4, 5));
+        }
+
+        public double CalculFanCapre(double days,double goats)
+        {
+            double kgFan = days * goats;
+
+            return kgFan;
+        }
+    }
+}
