1710a34d1b3acd23802564abd0ffb76fa5907402
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-15 20:12:21 +0200

    RezolvareCiuperci

---
+++ CiuperciProblem/CiuperciProblem/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CiuperciProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(15, CalculCiuperci(20,5));
+        }
+        public double CalculCiuperci(double totalMushrooms,double whiteMushrooms)
+        {
+            double coeficient = totalMushrooms / whiteMushrooms - 1;
+            double redMushrooms = coeficient * whiteMushrooms;
+
+            return redMushrooms;
+        }
+    }
+}
