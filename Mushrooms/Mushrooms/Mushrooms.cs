10b2c828e9c286f8f1221374ade3213c791a3558
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-16 17:49:02 +0200

    MushroomsProblemFINAL

---
+++ Mushrooms/Mushrooms/Mushrooms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class Mushrooms
+    {
+        [TestMethod]
+        public void CalculateMushrooms()
+        {
+            Assert.AreEqual(1, CalculateRedMushrooms(9, 3));
+        }
+        public double CalculateRedMushrooms(double totalMushrooms, double coefficient)
+        {
+            double whiteMushroomns = totalMushrooms / (coefficient + 1);
+            double redMushrooms = totalMushrooms - whiteMushroomns;
+
+            return redMushrooms;
+        }
+    }
+}
