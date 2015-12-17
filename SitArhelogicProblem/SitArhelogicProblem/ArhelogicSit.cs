5b86bfe5f580bad2e79ba3ecd995802ce3f73f55
pitulescubogdan <pitulescubogdan@gmail.com>
2015-12-17 15:48:27 +0200

    ArheologicSitFinal

---
+++ SitArhelogicProblem/SitArhelogicProblem/ArhelogicSit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SitArhelogicProblem
+{
+    [TestClass]
+    public class ArhelogicSit
+    {
+        [TestMethod]
+        public void CalculateTheArea()
+        {
+            Assert.AreEqual(14.5, CalculateArea(2,3,3,4,5,2));
+        }
+
+        public float CalculateArea(float firstCoordOfColOne,float secondCoordOfColOne,float firstCoordOfColTwo,float secondCoordOFColTwo,float firstCoordOfColThree,float secondCoordOfColThree)
+        {
+            float delta = (firstCoordOfColOne * secondCoordOFColTwo) + (firstCoordOfColTwo * secondCoordOfColThree) + (firstCoordOfColThree * secondCoordOfColOne);
+
+            return delta / 2;
+        }
+    }
+}
