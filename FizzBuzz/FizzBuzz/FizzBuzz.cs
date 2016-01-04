1726aab552eff45233320b2af9153459bd90e11a
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-04 19:00:53 +0200

    FizzBuzz problem

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzz
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Fizz", CheckMultiple(12));
+        }
+
+        public string CheckMultiple(int NumberInserted)
+        {
+            string result = "";
+            return result;
+        }
+    }
+}
