87c4517ef1e3cc80d394dee449e7373faa6eb824
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-04 20:28:29 +0200

    Rent for every penalty days

--- Rent/Rent/Rent.cs
+++ Rent/Rent/Rent.cs
@@ -7,14 +7,45 @@ namespace Rent
     public class Rent
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculateForFirstPenalty()
         {
-            Assert.AreEqual(251, CalculateMonthlyRate(250,10));
+            Assert.AreEqual(255, CalculateMonthlyRate(250,5));
         }
-        public decimal CalculateMonthlyRate(decimal MonthlyRate,int PenaltyDays)
+        [TestMethod]
+        public void CalculateForSecondPenalty()
         {
+            Assert.AreEqual(262.5, CalculateMonthlyRate(250, 15));
+        }
+        [TestMethod]
+        public void CalculateForThirdPenalty()
+        {
+            Assert.AreEqual(275, CalculateMonthlyRate(250, 35));
+        }
+        public double CalculateMonthlyRate(double MonthlyRate,int PenaltyDays)
+        {
+            double RentWithPenalty =0;
+            double firstPenalty = 0.02 * MonthlyRate;
+            double secondPenalty =0.05 * MonthlyRate;
+            double thirdPenalty = 0.1  * MonthlyRate;
+            if (PenaltyDays >= 1 && PenaltyDays <= 10)
+            {
+                RentWithPenalty = MonthlyRate + firstPenalty;
+            }
+            else if (PenaltyDays >= 11 && PenaltyDays <= 30)
+            {
+                RentWithPenalty = MonthlyRate + secondPenalty;
+            }
+            else if (PenaltyDays >= 31 && PenaltyDays <= 40)
+            {
+                RentWithPenalty = MonthlyRate + thirdPenalty;
+            }
+            else
+            {
+                RentWithPenalty = MonthlyRate;
+            }
 
-            return 0;
+            return RentWithPenalty;
+
         }
     }
 }
