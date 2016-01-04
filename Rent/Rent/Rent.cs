d5fd908ac7e36ff50ffdb222a06f1e203ff8af14
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-04 19:21:35 +0200

    MonthlyRent

---
+++ Rent/Rent/Rent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rent
+{
+    [TestClass]
+    public class Rent
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(251, CalculateMonthlyRate(250));
+        }
+        public decimal CalculateMonthlyRate(decimal MonthlyRate)
+        {
+
+            return 0;
+        }
+    }
+}
