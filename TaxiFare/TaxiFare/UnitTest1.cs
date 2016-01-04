535bc8c3347599f5016345f748e101fb8967405e
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-04 20:34:40 +0200

    TaxiFare

---
+++ TaxiFare/TaxiFare/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaxiFare
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(20, CalculateTaxiFare(15, 5));
+        }
+        public decimal CalculateTaxiFare(int distance, int pricePerKm)
+        {
+
+            return 0;
+        }
+    }
+}
