58310850ee3cea96e850f67c67a0b9ed82ade29d
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-04 18:44:18 +0200

    Watermelon Probl;em

---
+++ WatermelonProblem/WatermelonProblem/Watermelon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WatermelonProblem
+{
+    [TestClass]
+    public class Watermelon
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("DA", DivideWatermelon(50));
+        }
+        public string DivideWatermelon(int WatermelonWeight)
+        {
+            return "DA";
+        }
+    }
+}
