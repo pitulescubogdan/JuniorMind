5d20a4edeab4bb146c701ad1f7370694f2f81050
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-05 23:07:35 +0200

    RomanNumerals

--- RomanNumerals/RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals/RomanNumerals.cs
@@ -14,22 +14,9 @@ namespace RomanNumerals
 
         public string ToRoman(int numberInserted)
         {
-            string result = "";
-            string[] romanNumerals = {"", "V", "IV", "I" };
-            int[] numbers = {0, 5, 4, 1 };
+            
 
-           for(int i = 0; i < romanNumerals.Length; i++)
-            {
-                for(int j = 0; j < numbers.Length; j++)
-                {
-                    if(numberInserted - numbers[j]> 0)
-                    {
-                        result = romanNumerals[i];
-                    }
-                }
-            }
-
-            return result;               
+            return "I";               
         }
     }
 }
