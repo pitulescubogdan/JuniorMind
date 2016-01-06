1da6c81cdb3925803ef2c1781bd14d3f2a92b2a6
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-06 20:46:39 +0200

    Check for similiraties in both strings

--- PrefixLetters/PrefixLetters/PrefixLetters.cs
+++ PrefixLetters/PrefixLetters/PrefixLetters.cs
@@ -9,12 +9,25 @@ namespace PrefixLetters
         [TestMethod]
         public void CheckForPrefix()
         {
-            Assert.AreEqual("", checkPrefix("ab", "ac"));
+            Assert.AreEqual("aa", checkPrefix("ab", "aa"));
         }
         public string checkPrefix(string firstString,string secondString)
         {
+            string result="";
+            char constantForFirstArray;
+            string[] firstArray = { firstString };
 
-            return "";
+            for (int i = 0; i < firstString.Length; i++)
+                for (int j = 0; j < secondString.Length; j++)
+                {
+                    if (firstString[i] == secondString[i])
+                    {
+                        constantForFirstArray = firstString[i];
+                        result += constantForFirstArray;
+                    }
+                }
+
+            return result;
         }
     }
 }
