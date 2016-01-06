969d0fbdd61322c13f49b7ca937c6c56d83a9717
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-06 14:37:33 +0200

    Roman Numerals to roman

---
+++ RomanNumerals/RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumerals
+{
+    [TestClass]
+    public class RomanNumerals
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("I", ToRoman(1));
+        }
+        public string ToRoman(int numberInserted)
+        {
+            return "I";
+        }
+    }
+}
