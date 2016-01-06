14c737e809e9562e097d2cbc4e49c116b82f7f99
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-06 15:14:03 +0200

    ChessTable

---
+++ ChessSquares/ChessSquares/ChessSquares.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessSquares
+{
+    [TestClass]
+    public class ChessSquares
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, CalculateChessSquares(8));
+        }
+        public int CalculateChessSquares(int chessLength)
+        {
+            
+
+            return 0;
+        }
+    }
+}
