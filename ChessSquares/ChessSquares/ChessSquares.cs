ad28e010ba33e64a3b1446f45f1da8e1f3b66c6d
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-06 15:20:51 +0200

    CessLengh for the first square

--- ChessSquares/ChessSquares/ChessSquares.cs
+++ ChessSquares/ChessSquares/ChessSquares.cs
@@ -9,13 +9,20 @@ namespace ChessSquares
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1, CalculateChessSquares(8));
+            Assert.AreEqual(1, CalculateChessSquares(1));
         }
         public int CalculateChessSquares(int chessLength)
         {
+            int result=0;
+
+            while(!(chessLength==0))
+            {
+                result += chessLength * chessLength;
+                chessLength--;
+            }
             
 
-            return 0;
+            return result;
         }
     }
 }
