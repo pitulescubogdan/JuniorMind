6cdedfaca66fc4835d380dec864ca9ff8c8a6a48
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-06 21:03:52 +0200

    Panagram start

---
+++ Panagram/Panagram/Panagram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Panagram
+{
+    [TestClass]
+    public class Panagram
+    {
+        [TestMethod]
+        public void CheckIfPanagram()
+        {
+            Assert.AreEqual("", CheckForPanagram("The quick brown fox jumps over the lazy dog");
+        }
+        public string CheckForPanagram(string sentenceInserted)
+        {
+
+            return "";
+        }
+    }
+}
