3e7a73997b7a0d7fddc1facee01252f66a55d49a
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-06 20:15:43 +0200

    PrefixLetters start

---
+++ PrefixLetters/PrefixLetters/PrefixLetters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrefixLetters
+{
+    [TestClass]
+    public class PrefixLetters
+    {
+        [TestMethod]
+        public void CheckForPrefix()
+        {
+            Assert.AreEqual("", checkPrefix("ab", "ac"));
+        }
+        public string checkPrefix(string firstString,string secondString)
+        {
+
+            return "";
+        }
+    }
+}
