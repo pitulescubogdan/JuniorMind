4d3233d75e7fced1df1af9df97ae7cfcabe44aa1
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-07 15:41:05 +0200

    Test excel columns for the second and third interval

--- ExcelColumnsP/ExcelColumnsP/ExcelColumns.cs
+++ ExcelColumnsP/ExcelColumnsP/ExcelColumns.cs
@@ -17,16 +17,42 @@ namespace ExcelColumnsP
             Assert.AreEqual("O", CalculateTheExcelColumns(15));
 
         }
+        [TestMethod]
+        public void TestForSecondInterval()
+        {
+            Assert.AreEqual("AH", CalculateTheExcelColumns(34));
+        }
+        [TestMethod]
+        public void TestForThirdInterval()
+        {
+            Assert.AreEqual("BH", CalculateTheExcelColumns(60));
+
+        }
         public string CalculateTheExcelColumns(int numberColumn)
         {
+            int number;
             string output = "";
             String[] columns = {"A","B","C","D","E","F","G"
                     ,"H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
             for (int i = 1; i < columns.Length; i++)
-            {                                         
-                    output = columns[numberColumn - 1];                      
+            {
+                number = 0;
+                if (numberColumn > 52)
+                {
+                    number = numberColumn - 52;
+                    output = "B" + columns[number - 1];
+                }
+                else if(numberColumn > 26 )
+                {
+                    number = numberColumn - 26;
+                    output = "A" + columns[number - 1];
+
+                }else
+                {
+                    output = columns[numberColumn - 1];
+                }
             }
-            return output;
+            return output;          
         }
     }
 }
