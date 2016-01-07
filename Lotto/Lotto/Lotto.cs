22348cdf5efc3b87d14c574c7f48436cdb610c2d
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-07 22:13:50 +0200

    Lotto with probabilities

--- Lotto/Lotto/Lotto.cs
+++ Lotto/Lotto/Lotto.cs
@@ -7,9 +7,27 @@ namespace Lotto
     public class Lotto
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LottoForSixOfSixtyNine()
         {
-            Assert.AreEqual(0,00000007, CalculateChancesLotto(6,49));
+            Assert.AreEqual(7.15112384201851e-8, CalculateChancesLotto(6,49));
+        }
+        [TestMethod]
+        public void LottoForFiveOfSixtyNine()
+        {
+            Assert.AreEqual(5.244157484146912e-7, CalculateChancesLotto(5, 49));
+
+        }
+        [TestMethod]
+        public void LottoForFourOfSixtyNine()
+        {
+            Assert.AreEqual(4.719741735732221e-6, CalculateChancesLotto(4, 49));
+
+        }
+        [TestMethod]
+        public void LottoForFiveOfForty()
+        {
+            Assert.AreEqual(1.519738361843625e-6, CalculateChancesLotto(5, 40));
+
         }
         public double CalculateChancesLotto(int numbersGuessed,int amountOfNumbers)
         {
@@ -18,7 +36,7 @@ namespace Lotto
             combination = CalculateFactorial(amountOfNumbers)/
                 (CalculateFactorial(numbersGuessed)*CalculateFactorial(amountOfNumbers- numbersGuessed));
 
-            return 1/combination;
+            return 1 / combination;
         }
         public double CalculateFactorial(int numberForFactiorial)
         {
