fd84bd92273c21ba2e69681a349c3f7f21c991c8
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-07 13:01:57 +0200

    PrefixLetters final

--- PrefixLetters/PrefixLetters/PrefixLetters.cs
+++ PrefixLetters/PrefixLetters/PrefixLetters.cs
@@ -14,26 +14,39 @@ namespace PrefixLetters
         [TestMethod]
         public void CheckForPrefixForDifferentLengths()
         {
-            Assert.AreEqual("aaa", checkPrefix("aaab", "aaabbbaa"));
+            Assert.AreEqual("aaa", checkPrefix("aaab", "aaaabbbaa"));
         }
         [TestMethod]
         public void CheckForNonePrefix()
         {
-            Assert.AreEqual("", checkPrefix("baaaxaa", "xaaaabbbi"));
+            Assert.AreEqual("", checkPrefix("abaaxaa", "xbaaabbbi"));
         }
-        public string checkPrefix(string firstString,string secondString)
+        public string checkPrefix(string firstString, string secondString)
         {
-            string result="";
-
-            for (int i = 0; i < firstString.Length; i++)               
-                {
-                    if (firstString[i] == secondString[i] && !(firstString[i] != firstString[0]))
-                    {                                         
+            string result = "";
+            if (firstString.Length < secondString.Length)
+            {
+                for (int i = 0; i < firstString.Length; i++)
+                {                 
+                    if (firstString[i] == secondString[i])
+                    {
                         result += firstString[i];
-                    }
-                }
 
+                    }else break;
+                }
+            } else
+            {
+                for (int i = 0; i < secondString.Length; i++)
+                {
+                    if (firstString[i] == secondString[i])
+                    {
+                        result += firstString[i];
+                    }else break;
+                }
+            }
             return result;
         }
     }
 }
+    
+
