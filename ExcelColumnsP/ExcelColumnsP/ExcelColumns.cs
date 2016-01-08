b7e07756fa049a3c5ef770a9701b0a6ce01f4cfa
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-08 16:03:31 +0200

    Excel Column for A

--- ExcelColumnsP/ExcelColumnsP/ExcelColumns.cs
+++ ExcelColumnsP/ExcelColumnsP/ExcelColumns.cs
@@ -11,58 +11,19 @@ namespace ExcelColumnsP
         {
             Assert.AreEqual("A", CalculateTheExcelColumns(1));
         }
-        [TestMethod]
-        public void TestForAnyColumnInTheFirstStage()
-        {
-            Assert.AreEqual("Z", CalculateTheExcelColumns(26));
-
-        }
-        [TestMethod]
-        public void TestForSecondInterval()
-        {
-            Assert.AreEqual("AA", CalculateTheExcelColumns(52));
-        }
-        [TestMethod]
-        public void TestForThirdInterval()
-        {
-            Assert.AreEqual("BH", CalculateTheExcelColumns(60));
-
-        }
-        [TestMethod]
-        public void TestForAnyColumnAndInterval()
-        {
-            Assert.AreEqual("SF", CalculateTheExcelColumns(500));
-        }
+       
         public string CalculateTheExcelColumns(int numberColumn)
         {
-            int number;
-            int numberOfInterval = 1;
-            string output = "";
-            String[] columns = {"A","B","C","D","E","F","G"
-                    ,"H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+            string result = String.Empty;
 
-            numberOfInterval = numberColumn / 26;          
-            for (int i = 0; i < columns.Length; i++)
-            {
-                if (numberOfInterval != 0)
-                {
-                    number = 0;
-                    if (numberColumn > (numberOfInterval * 26))
-                    {
-                        number = numberColumn - (numberOfInterval * 26);
-                        output = columns[numberOfInterval-1] + columns[number-1];                       
-                    }
-                    else
-                    {
-                        output = columns[numberColumn-1];
-                    }
-                }
-                else
-                {
-                    output = columns[numberColumn-1];
-                }
-            }
-            return output;          
+            result = ReturnExcelColumn(numberColumn);
+            
+            return result;          
+        }
+        public String ReturnExcelColumn(int number)
+        {
+
+            return ((char)('A' + number - 1)).ToString();
         }
     }
 }
