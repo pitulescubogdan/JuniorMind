56679a5d5f386b1897b6fb3fdb33ce740129fe23
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-13 22:51:01 +0200

    Prefix letters with Math function

--- PrefixLetters/PrefixLetters/PrefixLetters.cs
+++ PrefixLetters/PrefixLetters/PrefixLetters.cs
@@ -9,25 +9,24 @@ namespace PrefixLetters
         [TestMethod]
         public void CheckForPrefix()
         {
-            Assert.AreEqual("a", checkPrefix("ab", "aa"));
+            Assert.AreEqual("a", CheckPrefix("ab", "aa"));
         }
         [TestMethod]
         public void CheckForPrefixForDifferentLengths()
         {
-            Assert.AreEqual("aaa", checkPrefix("aaab", "aaaabbbaa"));
+            Assert.AreEqual("aaa", CheckPrefix("aaab", "aaaabbbaa"));
         }
         [TestMethod]
         public void CheckForNonePrefix()
         {
-            Assert.AreEqual("", checkPrefix("abaaxaa", "xbaaabbbi"));
+            Assert.AreEqual("", CheckPrefix("abaaxaa", "xbaaabbbi"));
         }
-        public string checkPrefix(string firstString, string secondString)
+        public string CheckPrefix(string firstString, string secondString)
         {
             string result = "";
             int widerString = 0;
 
-            if (firstString.Length >= secondString.Length) widerString = firstString.Length;
-            else widerString = secondString.Length;
+            widerString = Math.Max(firstString.Length, secondString.Length);
 
                 for (int i = 0; i < widerString; i++)
                 {                 
