3b85abb6e6c062f796158b986b57300d6c620322
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-14 23:12:22 +0200

    Cub problem start

---
+++ Cube Problam/Cube Problam/Cube.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cube_Problam
+{
+    [TestClass]
+    public class Cube
+    {
+        [TestMethod]
+        public void CheckCube()
+        {
+            Assert.AreEqual(0, CalculateCube(1));
+        }
+        public int CalculateCube(int number)
+        {
+            return 0;
+        }
+    }
+}
