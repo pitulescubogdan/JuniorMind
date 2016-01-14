c48bd49ca0131c8818e0736ca10c21faa6fec5d6
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-15 01:17:36 +0200

    Cube for first k

--- Cube Problam/Cube Problam/Cube.cs
+++ Cube Problam/Cube Problam/Cube.cs
@@ -13,17 +13,18 @@ namespace Cube_Problam
         }
         public int CalculateCube(int numberOfCube)
         {
-
             int number = 1;
+            int aux = 0;
             int cubicNumber = number * number * number;
 
-            while (cubicNumber % 1000 != 888)
+            while (cubicNumber % 1000 != 888 || aux != numberOfCube)
             {
                 number++;
-                cubicNumber = number * number * number;             
+                cubicNumber = number * number * number;
+                
+
+                if (cubicNumber % 1000 == 888) aux++;
             }
-
-
             return number;
         }
         
