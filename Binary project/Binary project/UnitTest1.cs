60484ce1d0effec67c7b60737cbe14d5298a5ee8
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-16 12:05:31 +0200

    Binary start convert to byte

---
+++ Binary project/Binary project/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Binary_project
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DecimalToByte()
+        {
+            Assert.AreEqual(0, ToByte(0));
+        }
+        public byte ToByte(int number)
+        {
+
+            return 0;
+        }
+    }
+}
