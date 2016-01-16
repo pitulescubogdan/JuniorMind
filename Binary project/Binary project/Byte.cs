33e4e007ba59e53fdd2e1a28203dc61a284be49b
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-16 16:49:29 +0200

    Decimal to byte for five

--- Binary project/Binary project/Byte.cs
+++ Binary project/Binary project/Byte.cs
@@ -9,19 +9,24 @@ namespace Binary_project
         [TestMethod]
         public void DecimalToByte()
         {
-           CollectionAssert.AreEqual(new byte[] {1,0,0,0},(ToByteConversion(8)));
+           CollectionAssert.AreEqual(new byte[] {1,0,0},(ToByteConversion(4)));
+        }
+        [TestMethod]
+        public void DecimalToByteForFive()
+        {
+           CollectionAssert.AreEqual(new byte[] {1,0,1},(ToByteConversion(5)));            
         }
         public byte[] ToByteConversion(int number)
         {
-            byte[] bits = new byte[1];          
-            int size = 1;
+            byte[] bits = new byte[0];          
+            int size = 0;
             int i = 0;
-                while (number >= 1)
+                while (number != 0)
                 {
-                    Array.Resize(ref bits, size);
-                    bits[i] = (byte)(number % 2);                                       
-                    i++;                  
                     size++;
+                    Array.Resize(ref bits, size);                  
+                    bits[i] = (byte)(number % 2);                                       
+                    i++;                                    
                     number = number / 2;
                 }              
                 return revertBytes(bits);
@@ -30,7 +35,7 @@ namespace Binary_project
         {
             byte[] output = new byte[bitsInserted.Length];
             int k = 1;
-            for (int j = 0; j < bitsInserted.Length - 1; j++)               
+            for (int j = 0; j < bitsInserted.Length; j++)               
                 {
                     output[j] = bitsInserted[bitsInserted.Length - k];
                     k++;
