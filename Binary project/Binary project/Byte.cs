0160f9d4029bb93ad41ac2da7e37bb78e443cfca
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-16 15:56:44 +0200

    Convert any number with reverse array added

--- Binary project/Binary project/Byte.cs
+++ Binary project/Binary project/Byte.cs
@@ -9,19 +9,37 @@ namespace Binary_project
         [TestMethod]
         public void DecimalToByte()
         {
-           CollectionAssert.AreEqual(new byte[] {1,1,1},(ToByteConversion(7)));
+           CollectionAssert.AreEqual(new byte[] {1,0,0,0},(ToByteConversion(8)));
         }
         public byte[] ToByteConversion(int number)
         {
-            byte[] bits = new byte[3];      
+            byte[] bits = new byte[1];          
+            int size = 1;
             int i = 0;
                 while (number >= 1)
                 {
-                    bits[i] = (byte)(number % 2);
+                    Array.Resize(ref bits, size);
+                    bits[i] = (byte)(number % 2);                                       
+                    i++;                  
+                    size++;
                     number = number / 2;
-                    i++;
-                }           
-            return bits;
+                }              
+                return revertBytes(bits);
+        }
+        public byte[] revertBytes(byte[] bitsInserted)
+        {
+            byte[] output = new byte[bitsInserted.Length];
+            int k = 1;
+            for (int j = 0; j < bitsInserted.Length - 1; j++)               
+                {
+                    output[j] = bitsInserted[bitsInserted.Length - k];
+                    k++;
+                }
+            return output;
         }
     }
 }
+
+ 
+
+                        
