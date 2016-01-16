bb7fee1c28b1362978ef14dfb54972a83ef50eb8
pitulescubogdan <pitulescubogdan@gmail.com>
2016-01-16 14:35:57 +0200

    Byte conversion for first 8 numbers

---
+++ Binary project/Binary project/Byte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Binary_project
+{
+    [TestClass]
+    public class Byte
+    {
+        [TestMethod]
+        public void DecimalToByte()
+        {
+           CollectionAssert.AreEqual(new byte[] {1,1,1},(ToByteConversion(7)));
+        }
+        public byte[] ToByteConversion(int number)
+        {
+            byte[] bits = new byte[3];      
+            int i = 0;
+                while (number >= 1)
+                {
+                    bits[i] = (byte)(number % 2);
+                    number = number / 2;
+                    i++;
+                }           
+            return bits;
+        }
+    }
+}
