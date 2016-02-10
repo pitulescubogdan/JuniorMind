b4841f9f97617187c3734420e5e6a2d35cb410af
pitulescubogdan <pitulescubogdan@gmail.com>
2016-02-10 20:27:02 +0200

    Get password function created

---
+++ Password/Password/Password.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Password
+{
+    [TestClass]
+    public class Password
+    {
+        [TestMethod]
+        public void GetPasswordOfSmallCharacters()
+        {
+            Assert.AreEqual("",GetPassword(0));
+        }
+
+        public string GetPassword(int noOfChars)
+        {
+            return "";
+        }
+    }
+}
