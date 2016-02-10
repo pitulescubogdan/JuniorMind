d030dcfa256f6c688cb48904caae2bb8de333d52
pitulescubogdan <pitulescubogdan@gmail.com>
2016-02-10 20:47:08 +0200

    the struct with the options created

--- Password/Password/Password.cs
+++ Password/Password/Password.cs
@@ -11,7 +11,26 @@ namespace Password
         {
             Assert.AreEqual("",GetPassword(0));
         }
+        public struct Options
+        {
+            public int smallChars;
+            public int noOfUpperChars;
+            public int noOfNumbers;
+            public int noOfSymbols;
+            public bool notIncludedSimilarChars;
+            public bool notIncludedAmbigueChars;
 
+            public Options(int smallChars, int noOfUpperChars, int noOfNumbers, int noOfSymbols, bool notIncludedSimilarChars, bool notIncludedAmbigueChars)
+            {
+                this.smallChars = smallChars;
+                this.noOfUpperChars = noOfUpperChars;
+                this.noOfNumbers = noOfNumbers;
+                this.noOfSymbols = noOfSymbols;
+                this.notIncludedSimilarChars = notIncludedSimilarChars;
+                this.notIncludedAmbigueChars = notIncludedAmbigueChars;
+            }
+
+        }
         public string GetPassword(int noOfChars)
         {
             return "";
