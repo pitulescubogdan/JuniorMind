2a45168f2020a9a32c4a02c4374b85b5156289e4
pitulescubogdan <pitulescubogdan@gmail.com>
2016-02-21 19:18:41 +0200

    Shopping started

---
+++ Shopping/Shopping/Shopping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shopping
+{
+    [TestClass]
+    public class Shopping
+    {
+        struct List
+        {
+            int milk;
+            int bread;
+            int oil;
+            public List(int milk,int bread, int oil)
+            {
+                this.milk = milk;
+                this.bread = bread;
+                this.oil = oil;
+            }
+
+        }
+        [TestMethod]
+        public void ShoppingTest()
+        {
+            Assert.AreEqual(0, CalculateTotalPrice());
+        }
+        public int CalculateTotalPrice()
+        {
+            return 0;
+        }
+    }
+}
