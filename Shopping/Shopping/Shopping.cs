9be3a362ed59c956c4604032dc3f0638804e068f
pitulescubogdan <pitulescubogdan@gmail.com>
2016-02-24 22:03:00 +0200

    Calculate the cheapest price from the list of objects

--- Shopping/Shopping/Shopping.cs
+++ Shopping/Shopping/Shopping.cs
@@ -18,10 +18,16 @@ namespace Shopping
 
         }
         [TestMethod]
-        public void ShoppingTest()
+        public void GreatestPrice()
         {
             var shopping = new ShoppingList[] { new ShoppingList("bread",3.4),new ShoppingList("milk",3.7),new ShoppingList("oil",7) };
             Assert.AreEqual(14.1, CalculateTotalPrice(shopping));
+        }
+        [TestMethod]
+        public void CheapestObject()
+        {
+            var shopping = new ShoppingList[] { new ShoppingList("milk", 3.7), new ShoppingList("bread", 3.4), new ShoppingList("oil", 7) };
+            Assert.AreEqual(3.4, CalculateTheMinimumObject(shopping));
         }
         public double CalculateTotalPrice(ShoppingList[] shoppingList)
         {
@@ -29,6 +35,17 @@ namespace Shopping
             for (int i = 0; i < shoppingList.Length; i++)
             {
                 result += shoppingList[i].amount;
+            }
+            return result;
+        }
+        public double CalculateTheMinimumObject(ShoppingList[] shoppingObject)
+        {
+            ShoppingList firstObject = shoppingObject[0];
+            double result = 0;
+            for(int i = 0; i < shoppingObject.Length ; i++)
+            {
+                result = (firstObject.amount < shoppingObject[i].amount) ? result : shoppingObject[i].amount;
+                
             }
             return result;
         }
