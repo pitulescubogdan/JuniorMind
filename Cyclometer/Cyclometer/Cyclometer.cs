63cafc48680025947e82b3d17f78c9b6e9363216
pitulescubogdan <pitulescubogdan@gmail.com>
2016-02-28 23:54:38 +0200

    Calculate entire distance for two bikers

--- Cyclometer/Cyclometer/Cyclometer.cs
+++ Cyclometer/Cyclometer/Cyclometer.cs
@@ -10,9 +10,9 @@ namespace Cyclometer
         {
             public string name;
             public int rotationPerSecond;
-            public int diameter;
+            public double diameter;
 
-            public Cycle(string name,int rotationPerSecond,int diameter)
+            public Cycle(string name,int rotationPerSecond,double diameter)
             {
                 this.name = name;
                 this.rotationPerSecond = rotationPerSecond;
@@ -20,17 +20,25 @@ namespace Cyclometer
             }
         }
         [TestMethod]
-        public void TotalDistance()
+        public void TotalDistanceForOneParticipant()
         {
-            var participant = new Cycle[] { new Cycle("Bogdan", 250, 28) };
+            var participant = new Cycle[] { new Cycle("Bogdan", 250, 0.28) };
             Assert.AreEqual(439.6, CalculateTotalDistance(participant));
         }
+        [TestMethod]
+        public void CalculateEntireDistanceOfAllParticipants()
+        {
+            var participants = new Cycle[] { new Cycle("Bogdan", 250, 0.28), new Cycle("Mihai",300,0.28) };
+            Assert.AreEqual(967.12, CalculateTotalDistance(participants),1);
+        }
+        [TestMethod]
+     
         public double CalculateTotalDistance(Cycle[] participants)
         {
             double result = 0;
             for (int i = 0; i < participants.Length; i++)
             {
-                result += (2 * 3.14 * 0.28) * participants[i].rotationPerSecond;
+                result += (2 * 3.14 * participants[i].diameter) * participants[i].rotationPerSecond;
             }
             return result;
         }
