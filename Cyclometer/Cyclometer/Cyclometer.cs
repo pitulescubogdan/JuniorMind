076280de44541b000a99043697abc72aebe31dd7
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-01 00:13:42 +0200

    Cyclometer distance

--- Cyclometer/Cyclometer/Cyclometer.cs
+++ Cyclometer/Cyclometer/Cyclometer.cs
@@ -9,36 +9,36 @@ namespace Cyclometer
         public struct Cycle
         {
             public string name;
-            public int rotationPerSecond;
             public double diameter;
+            public double rotations;
+            public double second;
 
-            public Cycle(string name,int rotationPerSecond,double diameter)
+            public Cycle(string name,double diameter, double rotations, double second)
             {
                 this.name = name;
-                this.rotationPerSecond = rotationPerSecond;
+                this.rotations = rotations;
                 this.diameter = diameter;
+                this.second = second;
             }
         }
         [TestMethod]
         public void TotalDistanceForOneParticipant()
         {
-            var participant = new Cycle[] { new Cycle("Bogdan", 250, 0.28) };
-            Assert.AreEqual(439.6, CalculateTotalDistance(participant));
+            var participant = new Cycle[] { new Cycle("Bogdan", 3, 0.28,1) };
+            Assert.AreEqual(2.6376, CalculateTotalDistance(participant),0.001);
         }
         [TestMethod]
         public void CalculateEntireDistanceOfAllParticipants()
         {
-            var participants = new Cycle[] { new Cycle("Bogdan", 250, 0.28), new Cycle("Mihai",300,0.28) };
-            Assert.AreEqual(967.12, CalculateTotalDistance(participants),1);
-        }
-        [TestMethod]
-     
+            var participants = new Cycle[] { new Cycle("Bogdan", 3, 0.28,1), new Cycle("Mihai",2,0.28,1) };
+            Assert.AreEqual(4.396, CalculateTotalDistance(participants),0.001);
+        } 
         public double CalculateTotalDistance(Cycle[] participants)
         {
             double result = 0;
             for (int i = 0; i < participants.Length; i++)
             {
-                result += (2 * 3.14 * participants[i].diameter) * participants[i].rotationPerSecond;
+                result += 3.14 * participants[i].diameter * participants[i].rotations;
             }
             return result;
         }
