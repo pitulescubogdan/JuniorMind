f517ec544dff52e610626c02c0c047adc6e1268b
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-02 01:10:37 +0200

    Get the participant with maximum speed

--- Cyclometer/Cyclometer/Cyclometer.cs
+++ Cyclometer/Cyclometer/Cyclometer.cs
@@ -6,14 +6,14 @@ namespace Cyclometer
     [TestClass]
     public class Cyclometer
     {
-        public struct Cycle
+        public struct Participant
         {
             public string name;
             public double diameter;
             public double rotations;
             public double second;
 
-            public Cycle(string name,double diameter, double rotations, double second)
+            public Participant(string name,double diameter, double rotations, double second)
             {
                 this.name = name;
                 this.rotations = rotations;
@@ -24,23 +24,31 @@ namespace Cyclometer
         [TestMethod]
         public void TotalDistanceForOneParticipant()
         {
-            var participant = new Cycle[] { new Cycle("Bogdan", 3, 0.28,1) };
+            var participant = new Participant[] { new Participant("Bogdan", 3, 0.28,1) };
             Assert.AreEqual(2.6376, CalculateTotalDistance(participant),0.001);
         }
         [TestMethod]
         public void CalculateEntireDistanceOfAllParticipants()
         {
-            var participants = new Cycle[] { new Cycle("Bogdan", 3, 0.28,1), new Cycle("Mihai",2,0.28,1) };
+            var participants = new Participant[] { new Participant("Bogdan", 3, 0.28,1), new Participant("Mihai",2,0.28,1) };
             Assert.AreEqual(4.396, CalculateTotalDistance(participants),0.001);
         }
         [TestMethod]
         public void CalculateTheMaximumSpeed()
         {
-            var participants = new Cycle[] { new Cycle("Bogdan", 3, 0.28, 1), new Cycle("Mihai", 2, 0.28, 1) };
-            Assert.AreEqual(0.84, CalculateMaxSpeed(participants), 0.001);
-
+            var participants = new Participant[] { new Participant("Bogdan", 3, 0.28, 1), new Participant("Mihai", 2, 0.28, 1) };
+            var maxSpeed = new Participant("Bogdan", 3, 0.28, 1);
+            Assert.AreEqual(maxSpeed, CalculateMaxSpeed(participants));
         }
-        public double CalculateTotalDistance(Cycle[] participants)
+        [TestMethod]
+        public void CalculateMaxSpeedForMoreParticipants()
+        {
+            var participants = new Participant[] { new Participant("Bogdan", 3, 0.28, 1), new Participant("Mihai", 2, 0.28, 1),new Participant("Dragos",5,0.28,1) };
+            var maxSpeed = new Participant("Dragos", 5, 0.28, 1);
+            Assert.AreEqual(maxSpeed, CalculateMaxSpeed(participants));
+        }
+        
+        public double CalculateTotalDistance(Participant[] participants)
         {
             double result = 0;
             for (int i = 0; i < participants.Length; i++)
@@ -49,15 +57,20 @@ namespace Cyclometer
             }
             return result;
         }
-        public double CalculateMaxSpeed(Cycle[] participant)
+        public Participant CalculateMaxSpeed(Participant[] participant)
         {
+            Participant output = participant[0];
+            double firstSpeed = participant[0].diameter * participant[0].rotations;
+
             double result = 0;
-            for(int i = 0; i < participant.Length - 1; i++)
+            for(int i = 0; i < participant.Length; i++)
             {
                 double speed = participant[i].diameter * participant[i].rotations;
                 result = Math.Max(result, speed);
+                output = (firstSpeed > speed) ? output : participant[i];
             }
-            return result;
+            return output;
         }
+        
     }
 }
