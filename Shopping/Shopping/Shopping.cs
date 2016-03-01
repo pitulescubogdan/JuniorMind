d707bb82b24e66187da5c9cf776a8f8fddc10ed2
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-02 00:01:29 +0200

    Shopping refactorised

--- Shopping/Shopping/Shopping.cs
+++ Shopping/Shopping/Shopping.cs
@@ -6,11 +6,11 @@ namespace Shopping
     [TestClass]
     public class Shopping
     {
-        public struct ShoppingList
+        public struct Product
         {
             public string nameOfProduct;
             public double amount;
-            public ShoppingList(string nameOfProduct, double amount)
+            public Product(string nameOfProduct, double amount)
             {
                 this.nameOfProduct = nameOfProduct;
                 this.amount = amount;
@@ -20,60 +20,74 @@ namespace Shopping
         [TestMethod]
         public void GreatestPrice()
         {
-            var shopping = new ShoppingList[] { new ShoppingList("bread", 3.4), new ShoppingList("milk", 3.7), new ShoppingList("oil", 7) };
+            var shopping = new Product[] { new Product("bread", 3.4), new Product("milk", 3.7), new Product("oil", 7) };
             Assert.AreEqual(14.1, CalculateTotalPrice(shopping));
         }
         [TestMethod]
         public void CheapestObject()
         {
-            var shopping = new ShoppingList[] { new ShoppingList("milk", 3.7), new ShoppingList("bread", 3.4), new ShoppingList("oil", 7) };
-            Assert.AreEqual(new ShoppingList("bread", 3.4), CalculateTheMinimumObject(shopping));
+            var shopping = new Product[] { new Product("milk", 3.7), new Product("bread", 3.4), new Product("oil", 7) };
+            Assert.AreEqual(new Product("bread", 3.4), CalculateTheMinimumObject(shopping));
 
         }
         [TestMethod]
         public void HighestObject()
         {
-            var shopping = new ShoppingList[] { new ShoppingList("milk", 3.7), new ShoppingList("bread", 3.4), new ShoppingList("oil", 7) };
-            Assert.AreEqual(new ShoppingList("oil", 7), GetTheHighestPrice(shopping));
+            var shopping = new Product[] { new Product("milk", 3.7), new Product("bread", 3.4), new Product("oil", 7) };
+            Assert.AreEqual(2, GetMaxObjectPosition(shopping));
 
         }
         [TestMethod]
         public void AveragePrice()
         {
-            var shopping = new ShoppingList[] { new ShoppingList("milk", 3.7), new ShoppingList("bread", 3.4), new ShoppingList("oil", 7) };
+            var shopping = new Product[] { new Product("milk", 3.7), new Product("bread", 3.4), new Product("oil", 7) };
             Assert.AreEqual(4.7, CalculateAverageShopping(shopping));
         }
         [TestMethod]
         public void EliminateTheHighest()
         {
-            var shopping = new ShoppingList[] { new ShoppingList("milk", 3.7), new ShoppingList("bread", 3.4), new ShoppingList("oil", 7) };
+            var shopping = new Product[] { new Product("milk", 3.7), new Product("bread", 3.4), new Product("oil", 7) };
 
-            Assert.AreEqual(2,CountObjects(RemoveTheMostExpensive(shopping)));
+            Assert.AreEqual(2, CountObjects(RemoveTheMostExpensive(shopping)));
         }
         [TestMethod]
         public void EliminateTheHighestBetweenTwoObjects()
         {
-            var shopping = new ShoppingList[] { new ShoppingList("oil", 7), new ShoppingList("bread", 3.4),  };
-            CollectionAssert.AreEqual(new ShoppingList[] { new ShoppingList("bread", 3.4) }, 
+            var shopping = new Product[] { new Product("oil", 7), new Product("bread", 3.4) };
+            CollectionAssert.AreEqual(new Product[] { new Product("bread", 3.4) },
+                RemoveTheMostExpensive(shopping));
+        }
+        [TestMethod]
+        public void EliminateTheHighestBetweenThreeObjects()
+        {
+            var shopping = new Product[] { new Product("bread", 3.4), new Product("oil", 7), new Product("chocolate", 5) };
+            CollectionAssert.AreEqual(new Product[] { new Product("bread", 3.4), new Product("chocolate", 5) },
+                RemoveTheMostExpensive(shopping));
+        }
+        [TestMethod]
+        public void EliminateTheHighestBetweenMoreProducts()
+        {
+            var shopping = new Product[] { new Product("bread", 3.4), new Product("oil", 7), new Product("chocolate", 5), new Product("seeds", 4) };
+            CollectionAssert.AreEqual(new Product[] { new Product("bread", 3.4), new Product("seeds", 4), new Product("chocolate", 5) },
                 RemoveTheMostExpensive(shopping));
         }
         [TestMethod]
         public void AddObject()
         {
-            var actualShopping = new ShoppingList[] { new ShoppingList("chocolate", 5), new ShoppingList("honey", 15) };
-            var newObject = new ShoppingList("Oreo", 4);
-            Assert.AreEqual(3, CountObjects(AddObject(actualShopping,newObject)));
+            var actualShopping = new Product[] { new Product("chocolate", 5), new Product("honey", 15) };
+            var newObject = new Product("Oreo", 4);
+            Assert.AreEqual(3, CountObjects(AddObject(actualShopping, newObject)));
         }
         [TestMethod]
         public void AddAnObjectToChart()
         {
-            var actualShopping = new ShoppingList[] { new ShoppingList("chocolate", 5), new ShoppingList("honey", 15) };
-            var newObject = new ShoppingList("Oreo", 4);
-            var newListOfObjects = new ShoppingList[] { new ShoppingList("chocolate", 5), new ShoppingList("honey", 15), new ShoppingList("Oreo", 4) };
+            var actualShopping = new Product[] { new Product("chocolate", 5), new Product("honey", 15) };
+            var newObject = new Product("Oreo", 4);
+            var newListOfObjects = new Product[] { new Product("chocolate", 5), new Product("honey", 15), new Product("Oreo", 4) };
             CollectionAssert.AreEqual(newListOfObjects, AddObject(actualShopping, newObject));
-        }       
+        }
 
-        public double CalculateTotalPrice(ShoppingList[] shoppingList)
+        public double CalculateTotalPrice(Product[] shoppingList)
         {
             double result = 0;
             int counter = 0;
@@ -84,10 +98,10 @@ namespace Shopping
             }
             return result;
         }
-        public ShoppingList CalculateTheMinimumObject(ShoppingList[] shoppingObject)
+        public Product CalculateTheMinimumObject(Product[] shoppingObject)
         {
-            ShoppingList firstObject = shoppingObject[0];
-            ShoppingList result = shoppingObject[0];
+            Product firstObject = shoppingObject[0];
+            Product result = shoppingObject[0];
             for (int i = 0; i < shoppingObject.Length; i++)
             {
                 result = (firstObject.amount < shoppingObject[i].amount) ? result : shoppingObject[i];
@@ -95,39 +109,41 @@ namespace Shopping
             }
             return result;
         }
-        public double CalculateAverageShopping(ShoppingList[] shoppingObject)
+        public double CalculateAverageShopping(Product[] shoppingObject)
         {
             int counter = shoppingObject.Length;
 
             return CalculateTotalPrice(shoppingObject) / counter;
         }
-        public ShoppingList[] RemoveTheMostExpensive(ShoppingList[] shoppingObjects)
+        public Product[] RemoveTheMostExpensive(Product[] shoppingObject)
         {
+            shoppingObject[GetMaxObjectPosition(shoppingObject)] = shoppingObject[shoppingObject.Length - 1];
 
-            Array.Sort(shoppingObjects);
-            
-            Array.Resize(ref shoppingObjects, shoppingObjects.Length - 1);
+            Array.Resize(ref shoppingObject, shoppingObject.Length - 1);
 
-            return shoppingObjects;
+            return shoppingObject;
         }
-
-        public ShoppingList GetTheHighestPrice(ShoppingList[] shoppingObjects)
+        public int GetMaxObjectPosition(Product[] shoppingObjects)
         {
-            ShoppingList firstObject = shoppingObjects[0];
-            ShoppingList result = shoppingObjects[0];
+            Product firstObject = shoppingObjects[0];
+            int result = 0;
             for (int i = 0; i < shoppingObjects.Length; i++)
             {
-                result = (firstObject.amount > shoppingObjects[i].amount) ? result : shoppingObjects[i];
+                if (firstObject.amount < shoppingObjects[i].amount)
+                {
+                    result = i;
+                    firstObject = shoppingObjects[i];
+                }
 
             }
             return result;
         }
 
-        public int CountObjects(ShoppingList[] shoppingObjects)
+        public int CountObjects(Product[] shoppingObjects)
         {
             return shoppingObjects.Length;
         }
-        public ShoppingList[] AddObject(ShoppingList[] shoppingObjects,ShoppingList newObject)
+        public Product[] AddObject(Product[] shoppingObjects, Product newObject)
         {
             Array.Resize(ref shoppingObjects, shoppingObjects.Length + 1);
             shoppingObjects[shoppingObjects.Length - 1] = newObject;
