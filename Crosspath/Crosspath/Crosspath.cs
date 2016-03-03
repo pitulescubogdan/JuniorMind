3e9d5aed9de2437b81884b8547d2c8f47b32ff09
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-03 21:02:39 +0200

    Crosspath created and structure made

---
+++ Crosspath/Crosspath/Crosspath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crosspath
+{
+    [TestClass]
+    public class Crosspath
+    {
+        public struct Directions
+        {
+            public bool up;
+            public bool down;
+            public bool left;
+            public bool right;
+
+            public Directions(bool up, bool down, bool left, bool right)
+            {
+                this.up = up;
+                this.down = down;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        [TestMethod]
+        public void GetFirstIntersectionPoint()
+        {
+            var directions = new Directions[] { new Directions(true, false, false, true) };
+            Assert.AreEqual(0, ReturnFistIntersection(directions));
+        }
+
+        public int ReturnFistIntersection(Directions[] directions)
+        {
+            
+            return 0;
+        }
+    }
+}
