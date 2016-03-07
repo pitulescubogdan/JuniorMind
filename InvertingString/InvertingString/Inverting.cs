42b800dddf32397b0dbfbb35f1200ef42a0a1730
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-07 13:45:58 +0200

    Reverse string created

---
+++ InvertingString/InvertingString/Inverting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InvertingString
+{
+    [TestClass]
+    public class Inverting
+    {
+        [TestMethod]
+        public void ReverseStringTest()
+        {
+            Assert.AreEqual("cba", ReverseString("abc"));
+        }
+
+        public string ReverseString(string inputString)
+        {
+            return "cba";
+        }
+
+    }
+}
