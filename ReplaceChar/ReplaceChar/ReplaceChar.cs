e43459faacdd61d387c2d94710419d07780e888f
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-07 16:18:11 +0200

    Replace char with string started

---
+++ ReplaceChar/ReplaceChar/ReplaceChar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReplaceChar
+{
+    [TestClass]
+    public class ReplaceChar
+    {
+        [TestMethod]
+        public void CheckReplacement()
+        {
+            Assert.AreEqual("SALUTSALUTSALUT", ReplaceChars("abc","SALUT"));
+        }
+        public string ReplaceChars(string inputString,string wordToReplace)
+        {
+            return "SALUTSALUTSALUT";
+        }
+    }
+}
