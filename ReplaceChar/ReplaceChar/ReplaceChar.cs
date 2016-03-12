bd63f39f619d726b7237c2b333ad3d1e997cb582
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-12 17:25:56 +0200

    replace at the begenning and in the end of the string works

--- ReplaceChar/ReplaceChar/ReplaceChar.cs
+++ ReplaceChar/ReplaceChar/ReplaceChar.cs
@@ -26,7 +26,13 @@ namespace ReplaceChar
         public string ReplaceChars(string inputString,string wordToReplace,char charReplaced)
         {
             string result = string.Empty;
-            if (inputString.Length <= 1) return inputString;
+            
+            if (inputString.Length <= 1)
+            {
+                if(inputString[0].ToString().Contains(charReplaced))  return wordToReplace;
+                if (inputString[1].ToString().Contains(charReplaced)) return wordToReplace;
+                return inputString;
+            }
             if (inputString[inputString.Length - 1].ToString().Contains(charReplaced))
             {
                 return ReplaceChars(inputString.Substring(0, inputString.Length - 1), wordToReplace, charReplaced) + wordToReplace;
