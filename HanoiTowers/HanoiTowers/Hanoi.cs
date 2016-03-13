c39c26ec74c3a31a2ab155785298180be318f0db
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-13 23:26:00 +0200

    Hanoi Towers created

---
+++ HanoiTowers/HanoiTowers/Hanoi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HanoiTowers
+{
+    [TestClass]
+    public class Hanoi
+    {
+        [TestMethod]
+        public void HanoiTowers()
+        {
+            Assert.AreEqual(true, MoveDisks(3));
+        }
+        public bool MoveDisks(int disk)
+        {
+            return true;
+        }
+    }
+}
