a4d980cefa483c43c0a9464c6b0b18fa9d3a8d6a
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-17 17:04:29 +0200

    Calculator created

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class Calculator
+    {
+        [TestMethod]
+        public void CalculateThreeTimesFour()
+        {
+            Assert.AreEqual(12, Calculate("* 3 4 "));
+        }
+        public double Calculate(string inputString)
+        {
+            return 12;
+        }
+    }
+}
