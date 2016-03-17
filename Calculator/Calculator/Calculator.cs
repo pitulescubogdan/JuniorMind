634a3b1f69a17bfb28866bc7c4d0c427a5c0caaa
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-17 17:25:30 +0200

    Calculate one plus two

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -7,13 +7,26 @@ namespace Calculator
     public class Calculator
     {
         [TestMethod]
-        public void CalculateThreeTimesFour()
+        public void CalculateOnePlusTwo()
         {
-            Assert.AreEqual(12, Calculate("* 3 4 "));
+            Assert.AreEqual(3, Calculate("+ 1 2"));
         }
         public double Calculate(string inputString)
         {
-            return 12;
+            string[] elements = inputString.Split(' ');
+            string firstElement = elements[0];
+            double number;
+
+            if(Double.TryParse(elements[0],out number))
+            {
+                return number;
+            }
+
+            switch (firstElement)
+            {
+                case "+": return Calculate(elements[1]) + Calculate(elements[2]);
+                default: return Calculate(elements[1]) - Calculate(elements[2]);
+            }
         }
     }
 }
