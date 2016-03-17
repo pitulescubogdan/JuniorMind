224dfcff6758465cd993942692cc623a417a256b
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-17 18:43:15 +0200

    Pascal triangle created

---
+++ PascalTriangle/PascalTriangle/PascalTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PascalTriangle
+{
+    [TestClass]
+    public class PascalTriangle
+    {
+        [TestMethod]
+        public void Pascal()
+        {
+            Assert.AreEqual(1, GetPascal(1, 1));
+        }
+        
+        public int GetPascal(int row, int column)
+        {
+            
+            return 1;
+        }
+    }
+}
