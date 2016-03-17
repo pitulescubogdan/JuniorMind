98c8163aa5877c2f6c83116ab31b485b5c7fec6f
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-17 21:08:08 +0200

    Hanoi towers refactorised

--- HanoiTowers/HanoiTowers/Hanoi.cs
+++ HanoiTowers/HanoiTowers/Hanoi.cs
@@ -9,10 +9,10 @@ namespace HanoiTowers
         [TestMethod]
         public void MoveOneDisk()
         {
-            int[] source = { 1,2,3,4 };
+            int[] source = { 1, 2, 3, 4 };
             int[] destination = new int[source.Length];
             int[] auxiliar = new int[source.Length];
-            CollectionAssert.AreEqual(new int[] { 1,2,3,4 }, MoveDisks(4,source,destination,auxiliar));
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4 }, MoveDisks(4, source, destination, auxiliar));
         }
         [TestMethod]
         public void MoveTwoDisks()
@@ -24,19 +24,24 @@ namespace HanoiTowers
         }
         public int[] MoveDisks(int disk, int[] source, int[] destination, int[] auxiliar)
         {
-            if(disk == 1)
+            if (disk == 1)
             {
-                destination[disk - 1] = source[disk - 1];
-                Array.Resize(ref source, source.Length - 1);
+                return MoveDisk(disk, source, destination);
             }
             else
             {
                 MoveDisks(disk - 1, source, auxiliar, destination);
-                destination[disk - 1] = source[disk - 1];
-                Array.Resize(ref source, source.Length - 1);
+                MoveDisk(disk, source, destination);
                 MoveDisks(disk - 1, auxiliar, destination, source);
             }
             return destination;
+        }
+
+        private static int[] MoveDisk(int disk, int[] source, int[] destination)
+        {
+            destination[disk - 1] = source[disk - 1];
+            Array.Resize(ref source, source.Length - 1);
+            return source;
         }
     }
 }
