e5bf884a269a219a1847c6bc9d5aa078358fd5b1
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-17 16:58:22 +0200

    Move two disks

--- HanoiTowers/HanoiTowers/Hanoi.cs
+++ HanoiTowers/HanoiTowers/Hanoi.cs
@@ -14,6 +14,14 @@ namespace HanoiTowers
             int[] auxiliar = new int[source.Length];
             CollectionAssert.AreEqual(new int[] { 1,0,0 }, MoveDisks(3,source,destination,auxiliar));
         }
+        [TestMethod]
+        public void MoveTwoDisks()
+        {
+            int[] source = { 1, 2, 3 };
+            int[] destination = new int[source.Length];
+            int[] auxiliar = new int[source.Length];
+            CollectionAssert.AreEqual(new int[] { 0, 2, 0 }, MoveDisks(3, source, destination, auxiliar));
+        }
         public int[] MoveDisks(int disk, int[] source, int[] destination, int[] auxiliar)
         {
             if(disk == 1)
@@ -24,8 +32,11 @@ namespace HanoiTowers
             else
             {
                 MoveDisks(disk - 1, source, auxiliar, destination);
+                destination[disk - 1] = source[disk - 1];
+                Array.Resize(ref source, source.Length - 1);
+
             }
-            return destination;
+            return auxiliar;
         }
     }
 }
