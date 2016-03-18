0dd5555e0f6945335c8b5a1aca46eff8cbad4cbe
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-18 15:33:44 +0200

    Pascal for seventh row

--- PascalTriangle/PascalTriangle/PascalTriangle.cs
+++ PascalTriangle/PascalTriangle/PascalTriangle.cs
@@ -21,28 +21,34 @@ namespace PascalTriangle
         [TestMethod]
         public void PascalForFourthRow()
         {
-            CollectionAssert.AreEqual(new int[] { 1, 3,3, 1 }, GetPascal(4));
+            CollectionAssert.AreEqual(new int[] { 1, 3, 3, 1 }, GetPascal(4));
+
+        }
+        [TestMethod]
+        public void PascalForSeventhRow()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 6, 15, 20, 15, 6, 1 }, GetPascal(7));
 
         }
 
         public int[] GetPascal(int row)
         {
             int[] output = new int[row];
-            if (row == 1 )
+            if (row == 1)
             {
                 return new int[] { 1 };
             }
             output[0] = 1;
             output[row - 1] = 1;
             int[] beforeRow = GetPascal(row - 1);
-            for (int i = 1;i< row - 1; i++)
+            for (int i = 1; i < row - 1; i++)
             {
-                output[i] = beforeRow[i-1] + beforeRow[i];
+                output[i] = beforeRow[i - 1] + beforeRow[i];
             }
-           
+
             return output;
         }
-       
+
         public int GetNumber(int[] number)
         {
             return number[0];
