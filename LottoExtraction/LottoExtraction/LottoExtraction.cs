c2708dcd91c250bf25e48fe31f007f7ae3f72312
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-19 07:56:42 +0200

    Sort lotto extractions created

---
+++ LottoExtraction/LottoExtraction/LottoExtraction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LottoExtraction
+{
+    [TestClass]
+    public class LottoExtraction
+    {
+        [TestMethod]
+        public void TestSort()
+        {
+            CollectionAssert.AreEqual(new int[] { 0 }, SortNumbers(1, 10, 5));
+        }
+        public int[] SortNumbers(int start, int end, int amount)
+        {
+            return new int[] { 0 };
+        }
+    }
+}
