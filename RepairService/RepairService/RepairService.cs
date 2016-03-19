f2acfa436a0e6331273e6cbc703c7e1d053d2cac
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-19 09:19:45 +0200

    Repair service created

---
+++ RepairService/RepairService/RepairService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepairService
+{
+    [TestClass]
+    public class RepairService
+    {
+        [TestMethod]
+        public void OneIssue()
+        {
+            Issue oneIssue = new Issue("Gasoline leaking", "High");
+            CollectionAssert.AreEqual(new string[] { "High" }, SortByPriority(oneIssue));
+        }
+
+        public struct Issue
+        {
+            string nameOfTheIssue;
+            string priority;
+            public Issue(string nameOfIssue, string priority)
+            {
+                this.nameOfTheIssue = nameOfIssue;
+                this.priority = priority;
+            }
+        }
+
+        public string[] SortByPriority(Issue issue)
+        {
+            return new string[] { "High" };
+        }
+
+    }
+}
