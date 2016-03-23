5af1952badccdcb325a0e4d624a249af7ae82a11
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-23 17:15:05 +0200

    Sorted words but need to remove the same occurences of a word within a string

--- Words/Words/Words.cs
+++ Words/Words/Words.cs
@@ -9,32 +9,45 @@ namespace Words
         [TestMethod]
         public void ArrangedWords()
         {
-            CollectionAssert.AreEqual(new string[]{ "empty" }, OrdinateWords("A positive person has positive energy"));
+            string[] result = { "positive", "positive", "A", "person", "has", "energy" };
+            CollectionAssert.AreEqual(result, OrdinateWords("A positive person has positive energy"));
         }
         [TestMethod]
         public void Occurences()
         {
             string text = "A positive person has positive energy";
-            Assert.AreEqual(2, GetOccurences(text, "positive"));
+            string[] check = text.Split(' ');
+            Assert.AreEqual(2, GetOccurences(check, "positive"));
         }
 
         public string[] OrdinateWords(string inputText)
         {
             string[] words = inputText.Split(' ');
-
-            
-
-            return new string[] { "empty" };
+            int length = words.Length;
+            while (length != 0)
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    if (GetOccurences(words, words[i - 1]) < GetOccurences(words, words[i])) Swap(words, i - 1, i);
+                }
+                length--;
+            }
+            return words;
         }
-        public int GetOccurences(string inputArray, string wordToSearch)
+        public int GetOccurences(string[] inputText, string wordToSearch)
         {
             int count = 0;
-            string[] words = inputArray.Split(' ');
-            for(int i =0;i<words.Length;i++)
+            for (int i = 0; i < inputText.Length; i++)
             {
-                if (wordToSearch == words[i]) count++;
+                if (wordToSearch == inputText[i]) count++;
             }
             return count;
+        }
+        public void Swap(string[] text, int indexToReplace, int indexReplacement)
+        {
+            var holder = text[indexToReplace];
+            text[indexToReplace] = text[indexReplacement];
+            text[indexReplacement] = holder;
         }
     }
 }
