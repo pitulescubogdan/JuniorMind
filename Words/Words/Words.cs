7a8090137758cf26548c871433484e4d03cf00e5
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-23 14:27:18 +0200

    Get occurences for a given word

--- Words/Words/Words.cs
+++ Words/Words/Words.cs
@@ -11,10 +11,30 @@ namespace Words
         {
             CollectionAssert.AreEqual(new string[]{ "empty" }, OrdinateWords("A positive person has positive energy"));
         }
+        [TestMethod]
+        public void Occurences()
+        {
+            string text = "A positive person has positive energy";
+            Assert.AreEqual(2, GetOccurences(text, "positive"));
+        }
+
         public string[] OrdinateWords(string inputText)
         {
+            string[] words = inputText.Split(' ');
+
+            
 
             return new string[] { "empty" };
+        }
+        public int GetOccurences(string inputArray, string wordToSearch)
+        {
+            int count = 0;
+            string[] words = inputArray.Split(' ');
+            for(int i =0;i<words.Length;i++)
+            {
+                if (wordToSearch == words[i]) count++;
+            }
+            return count;
         }
     }
 }
