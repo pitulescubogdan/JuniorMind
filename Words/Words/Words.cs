02afdc3d0c0a1976c8b0e92f72335675ce6e8a82
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-23 13:45:51 +0200

    Arrangament of the words apparition  within a text

---
+++ Words/Words/Words.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Words
+{
+    [TestClass]
+    public class Words
+    {
+        [TestMethod]
+        public void ArrangedWords()
+        {
+            CollectionAssert.AreEqual(new string[]{ "empty" }, OrdinateWords("A positive person has positive energy"));
+        }
+        public string[] OrdinateWords(string inputText)
+        {
+
+            return new string[] { "empty" };
+        }
+    }
+}
