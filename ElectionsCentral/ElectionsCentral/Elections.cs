febd124952c497545ad5cde52936abe0a27eb386
pitulescubogdan <pitulescubogdan@gmail.com>
2016-03-30 19:39:21 +0300

    Elections crated

---
+++ ElectionsCentral/ElectionsCentral/Elections.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ElectionsCentral
+{
+    [TestClass]
+    public class Elections
+    {
+        [TestMethod]
+        public void CentraliseElections()
+        {
+            Candidates[] list = new Candidates[]
+            {
+                new Candidates("Bogdan",100)
+            };
+            CollectionAssert.AreEqual(list, GetElections(list));
+        }
+
+        public struct Candidates
+        {
+            string name;
+            int noOfVotes;
+            public Candidates(string name, int votes)
+            {
+                this.name = name;
+                this.noOfVotes = votes;
+            }
+        }
+        public Candidates[] GetElections(Candidates[] candidates)
+        {
+            return candidates;
+        }
+    }
+}
