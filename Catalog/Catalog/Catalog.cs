6375c22e716c15a8a56e65cc1715da3eb111d680
pitulescubogdan <pitulescubogdan@gmail.com>
2016-04-03 12:04:05 +0300

    Average marks function created

--- Catalog/Catalog/Catalog.cs
+++ Catalog/Catalog/Catalog.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Catalog
 {
     [TestClass]
-    public class UnitTest1
+    public class Catalog
     {
         [TestMethod]
         public void OrderAlphabetical()
@@ -24,6 +24,26 @@ namespace Catalog
             CollectionAssert.AreEqual(sorted, SortAplhabetical(pupils));
 
         }
+        [TestMethod]
+        public void OrderByAvgMarks()
+        {
+            Pupil[] pupils = new Pupil[]
+            {
+                new Pupil("Bogdan", new Classes[] {new Classes("Info",new int[] {8,10,9 }) }),
+                new Pupil("Mihai",new Classes[] { new Classes("Mate",new int[] {9,9,10 }) }),
+                new Pupil("Andrei", new Classes[] { new Classes("Sport",new int[] { 10, 10, 10 })})
+            };
+            Pupil[] sorted = new Pupil[]
+            {
+                pupils[2],
+                pupils[1],
+                pupils[0]
+            };
+
+            CollectionAssert.AreEqual(sorted, SortByAvgMarks(pupils));
+
+        }
+
         public struct Pupil
         {
             public string name;
@@ -51,16 +71,22 @@ namespace Catalog
                 int k = i + 1;
                 while (k > 0)
                 {
-                    if (pupils[k - 1].name[0] > pupils[k].name[0])
-                    {
-                        var temp = pupils[k - 1];
-                        pupils[k - 1] = pupils[k];
-                        pupils[k] = temp;
-                    }
+                    if (pupils[k - 1].name[0] > pupils[k].name[0]) SwapPupils(pupils, k);
                     k--;
                 }
             }
             return pupils; ;
+        }
+        public Pupil[] SortByAvgMarks(Pupil[] pupils)
+        {
+            return pupils;
+        }
+
+        private static void SwapPupils(Pupil[] pupils, int k)
+        {
+            var temp = pupils[k - 1];
+            pupils[k - 1] = pupils[k];
+            pupils[k] = temp;
         }
     }
 }
