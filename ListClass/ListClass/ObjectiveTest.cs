5ce09a6a16386f1ebb612d7a8f4d180e9b1cfb92
pitulescubogdan <pitulescubogdan@gmail.com>
2016-04-04 23:46:34 +0300

    List Class created

---
+++ ListClass/ListClass/ObjectiveTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ListClass
+{
+    [TestClass]
+    public class ObjectiveTest
+    {
+        [TestMethod]
+        public void TestCount()
+        {
+            List<string> objectOne = new List<string>();
+
+            Assert.AreEqual(0, objectOne.Count);
+
+        }
+
+    }
+}
