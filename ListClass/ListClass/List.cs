fdd220219bb66622ae1a672fd82749791780ef31
pitulescubogdan <pitulescubogdan@gmail.com>
2016-04-06 22:50:20 +0300

    Add function and contains function created and tested

--- ListClass/ListClass/List.cs
+++ ListClass/ListClass/List.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ListClass
+namespace List
 {
     public class List<T> : IList<T>
     {
+        T[] stored = new T[1];
+
         public T this[int index]
         {
             get
@@ -26,7 +28,7 @@ namespace ListClass
         {
             get
             {
-                return 0;
+                throw new NotImplementedException();
             }
         }
 
@@ -40,7 +42,18 @@ namespace ListClass
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < stored.Length; i++)
+            {
+                if (i == stored.Length - 1)
+                {
+                    Array.Resize(ref stored, stored.Length * 2);
+                }
+                if (stored[i].Equals(0))
+                {
+                    stored[i] = item;
+                    return;
+                }
+            }
         }
 
         public void Clear()
@@ -50,7 +63,11 @@ namespace ListClass
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < stored.Length; i++)
+            {
+                if (stored[i].Equals(item)) return true;
+            }
+            return false;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
---
+++ ListClass/ListClass/List.tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace List
+{
+    [TestClass]
+    public class List
+    {
+        [TestMethod]    
+        public void ListCount()
+        {
+            List<string> objectOne = new List<string>();
+
+            Assert.AreEqual(0, objectOne.Count);
+        }
+        [TestMethod]
+        public void ListAdd()
+        {
+            List<int> objectOne = new List<int>();
+            objectOne.Add(2);
+            Assert.IsTrue(objectOne.Contains(2));
+        }
+        
+    }
+}
