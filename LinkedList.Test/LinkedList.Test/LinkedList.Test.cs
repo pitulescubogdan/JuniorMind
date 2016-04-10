722d2a37194f6fc1439cb44ad901e90f1ea4901c
pitulescubogdan <pitulescubogdan@gmail.com>
2016-04-10 12:58:30 +0300

    Linked list created and tested for empty list

---
+++ LinkedList.Test/LinkedList.Test/LinkedList.Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace LinkedList.Test
+{
+    public class LinkedListTest
+    {
+        [Fact]
+        public void LinkedListTestEmpty()
+        {
+            var list = new LinkedLists<int>();
+            Assert.True(list.Empty);
+            Assert.Equal(0, list.Count);
+        }
+    }
+}
---
+++ LinkedList.Test/LinkedList.Test/LinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Test
+{
+    public class LinkedLists<T> : LinkedList<T>
+    {
+        private Node head;
+        private int size;
+
+        public LinkedLists()
+        {
+            this.head = null;
+            this.size = 0;
+        }
+
+        public bool Empty
+        {
+            get { return this.size == 0; }
+
+        }
+        public new int Count
+        {
+            get { return this.size; }
+        }
+
+    }
+}
